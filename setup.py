5d7d14d4d53c54131e0478238219c8284d86e9e8
augusttell <august.andersen@outlook.com>
2021-01-10 21:51:11 +0100

    Inital commit for lint.

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Setup for the tradingapplication package."""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="tradingapi",
+    packages=find_packages(),
+    version="0.1.0",
+    description="This repo defines define the connection to the core trading apis.",
+    author="August Andersen",
+    license="",
+    python_requires=">=3.8"
+)
