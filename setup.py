7e1c497a336c3961c66d2b161a92d9a39ab8d0e3
augusttell <august.andersen@outlook.com>
2021-02-21 15:33:45 +0100

    update versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name="tradingapi",
     packages=find_packages(),
-    version="0.5.0",
+    version="0.6.0",
     description="This repo defines define the connection to the core trading apis.",
     author="August Andersen",
     license="",
