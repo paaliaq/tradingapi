c66d4f08fd0a881e5bc3e16a219ee632121f53b1
Maximilian <maximilian@pfundstein.org>
2022-03-23 19:59:47 +0100

    Adjust error path

--- src/mappers/closed_positions_mapper.py
+++ src/mappers/closed_positions_mapper.py
@@ -2,6 +2,7 @@
 from typing import Any
 
 from domainmodels.closed_position import ClosedPosition, ClosedPositionError
+
 from mappers.mapper import Mapper
 
 
@@ -14,25 +15,21 @@ class ClosedPositionMapper(Mapper[Any, ClosedPosition]):
         closed_position = ClosedPosition()
         closed_position.symbol = position.symbol
         closed_position.http_status_code = int(position.status)
+        closed_position.error = None
 
         try:
             closed_position.order = position.body
         except Exception:
             closed_position.order = None
 
-        try:
             closed_position.error = ClosedPositionError()
 
             # These two fields seem wrong as well
-            # closed_position.error.available = int(position.body.available)
-            # closed_position.error.code = int(position.body.available)
+            closed_position.error.available = int(position.body.available)
+            closed_position.error.code = 0
             closed_position.error.existing_qty = int(position.body.existing_qty)
             closed_position.error.held_for_orders = int(position.body.held_for_orders)
             closed_position.error.message = position.body.held_for_orders.message
             closed_position.error.symbol = position.body.symbol
-        except Exception:
-            # This error path is actually wrong
-            # If error non-parsable -> exception!
-            closed_position.error = None
 
         return closed_position
