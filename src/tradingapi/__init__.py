2e6e65b9ab49c98ee2e875dad125a011d55d6cbf
Maximilian <maximilian@pfundstein.org>
2022-03-23 22:04:57 +0100

    wip

--- src/tradingapi/__init__.py
+++ src/tradingapi/__init__.py
@@ -1,2 +1,4 @@
 """Package for placing trades through various trading APIs."""
+# TODO: Move to trading api and then everything else to core
+# Maybe that solves the import issues
 from ..domainmodels import *  # noqa: F401, F403
